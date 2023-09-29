29781392cc446f8c647513e191e1f9f23892030c
priti168 <pritiparmar168@gmail.com>
2023-09-29 03:46:29 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,7 +138,6 @@ class TestAccountService(TestCase):
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-    
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
